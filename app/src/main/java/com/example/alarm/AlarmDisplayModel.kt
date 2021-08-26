b4b4ff5f0fc3a4a26b5d667a8a32d134f7c3fecb
SeokJiWon1207 <syt1114@naver.com>
2021-08-26 19:52:52 +0900

    App의 View를 처리해주기 위한 Data Class 작성, 나타날 숫자를 format하기 위해 Data Class안에 getter를 이용한 형식 format 작성, 메인 layout 작성, sharedPreference를 통한 데이터 저장 함수 작성+

---
+++ app/src/main/java/com/example/alarm/AlarmDisplayModel.kt
@@ -0,0 +1,27 @@
+package com.example.alarm
+
+data class AlarmDisplayModel(
+    val hour: Int,
+    val minute: Int,
+    var onOff: Boolean
+) {
+    // 위에서 받은 인자 hour를 getter를 통해 조건에 의해 다시 반환함
+    val timeText: String
+        get() {
+            val h = "%02d".format(if (hour < 12) hour else hour - 12) // 앞자리 두자리로 포맷
+            val m = "%02d".format(minute) // 분 단위는 그대로 format
+
+            return "$h:$m"
+        }
+
+    // View의 AM/PM을 구분하기 위해 다시 return
+    val ampmText: String
+        get() {
+            return if (hour < 12) "AM" else "PM"
+        }
+
+    fun makeDataForDB(): String {
+
+        return "$hour:$minute"
+    }
+}
--- app/src/main/java/com/example/alarm/MainActivity.kt
+++ app/src/main/java/com/example/alarm/MainActivity.kt
@@ -1,11 +1,69 @@
 package com.example.alarm
 
+import android.app.TimePickerDialog
+import android.content.Context
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
+import java.util.*
 
+/*
+ step1 View를 초기화해주기
+ step2 데이터 가져오기
+ step3 View에 데이터 뿌려주기
+ */
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        initOnOffButton()
+        initChangeAlarmTimeButton()
+
+
+    }
+
+    private fun initOnOffButton() {
+        val onOffButton = findViewById<Button>(R.id.onOffButton)
+        onOffButton.setOnClickListener {
+            // 데이터를 확인
+            // On/Off 에 따라 작업을 처리 Off -> 제거, On -> 알람을 등록
+
+        }
+    }
+
+     /*1. 현재시간을 가져온다
+     2. TimePickDialog 띄워줘서 시간을 설정을 하도록 하게끔 하고, 그 시간을 가져와서
+     3. 데이터를 저장한다.
+     4. View를 업데이트한다.
+     5. 기존에 있던 알람을 삭제한다.*/
+    private fun initChangeAlarmTimeButton() {
+        val changeAlarmButton = findViewById<Button>(R.id.changeAlarmTimeButton)
+        changeAlarmButton.setOnClickListener {
+
+            val calendar = Calendar.getInstance()
+            TimePickerDialog(this, { picker, hour, minute ->
+
+                val model = saveAlarmModel(hour, minute, false)
+            }, calendar.get(Calendar.HOUR_OF_DAY), calendar.get(Calendar.MINUTE), false )
+                .show()
+        }
+    }
+
+    private fun saveAlarmModel(hour: Int, minute: Int, onOff: Boolean): AlarmDisplayModel {
+        val model = AlarmDisplayModel(
+            hour = hour,
+            minute = minute,
+            onOff = false
+        )
+
+        val sharedPreference = getSharedPreferences("time", Context.MODE_PRIVATE)
+
+        with(sharedPreference.edit()) {
+            putString("alarm",model.makeDataForDB())
+            putBoolean("onOff",model.onOff)
+            commit() // or apply()
+        }
+        return model
     }
 }
